42b64cf48199399a417d0325312f39e2036aa91f
jennioksman <c3okje00@students.oamk.fi>
2024-11-21 10:55:11 +0200

    Init

---
+++ movieservice.js
@@ -0,0 +1,31 @@
+import express from 'express'
+
+var app = express()
+
+app.listen(3001, () => {
+    console.log('Server in running...')
+})
+
+app.get('/', (req,res)=> {
+
+})
+
+app.get('/user', (req,res)=> {
+    
+})
+
+app.get('/movie', (req,res)=> {
+    
+})
+
+app.get('/genre', (req,res)=> {
+    
+})
+
+app.get('/favorite_movie', (req,res)=> {
+    
+})
+
+app.get('/rating', (req,res)=> {
+    
+})
