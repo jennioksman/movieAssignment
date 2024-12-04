60feaed102effaf7637846bb27a9e9c6425a5c0f
jennioksman <c3okje00@students.oamk.fi>
2024-12-04 18:20:06 +0200

    FInal endpoints added

--- movieservice.js
+++ movieservice.js
@@ -7,38 +7,86 @@ app.listen(3001, () => {
     console.log('Server in running...')
 })
 
-getMovies()
 
-async function getMovies() { 
+app.post('/user/register', async (req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+app.post('/movie', async (req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+app.get('/movie', async (req,res)=> {
     try{
         const response = await pgPool.query("SELECT * FROM movie ")
-        console.log(response)
+        res.json(response.rows)
+    }catch(err){
+        console.log(err.message)
+    }  
+})
+
+app.get('/movie/id', async (req,res)=> {
+    try{
+        
+    }catch(err){
+        
+    }  
+})
+
+app.get('/movie/keyword', async (req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+app.delete('/movie/id', async (req,res) => {
+    try{
+        const response = await pgPool.query("SELECT * FROM movie ")
+        res.json(response.rows)
     }catch(err){
         console.log(err.message);
     }  
-}
-
-app.get('/', (req,res)=> {
-
 })
 
-app.get('/user', (req,res)=> {
- 
-})
-
-app.get('/movie', (req,res)=> {
+app.post('/genre', async (req,res) => {
+    try {
         
+    } catch (error) {
+        
+    }
 })
 
-app.get('/genre', (req,res)=> {
-    
+app.get('/user/{username}/favorite_movie', async (req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
 })
 
-app.get('/favorite_movie', (req,res)=> {
-    
+app.post('/user/{username}/favorite_movie', async (req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
 })
 
-app.get('/rating', (req,res)=> {
-    
+app.post('/rating', async (req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
 })
 
